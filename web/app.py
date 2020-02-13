86009ae33f3bfef27273c2ff6600cc5b3d9b331c
SinghoiChoi <380309472@qq.com>
2020-02-13 20:41:57 +0800

    编写MVC，修复app.py的错误

--- web/app.py
+++ web/app.py
@@ -111,7 +111,7 @@ async def response_factory(app, handler):
                 resp.content_type = "application/json;charset=utf-8"
                 return resp
             else:
-                resp = web.Response(body=app["__template__"].get_template(template).render(**r).encode("utf8"))
+                resp = web.Response(body=app["__templating__"].get_template(template).render(**r).encode("utf8"))
                 resp.content_type = "text/html;charset=utf-8"
                 return resp
         if isinstance(r, int) and 100 <= r < 600:
@@ -121,7 +121,8 @@ async def response_factory(app, handler):
             if isinstance(r, int) and 100 <= r < 600:
                 return web.Response(t, str(m))
         resp = web.Response(body=str(r).encode("utf8"))
-        resp.content_type = "text/plaiin;charset=utf-8"
+        resp.content_type = "text/plain;charset=utf-8"
+        return resp
 
     return response
 
--- web/handlers.py
+++ web/handlers.py
@@ -1,1 +1,12 @@
 """Url Handlers"""
+from coroweb import get
+from models import User
+
+
+@get("/")
+async def index(request):
+    users = await User.find_all()
+    return {
+        "__template__": "test.html",
+        "users": users
+    }
